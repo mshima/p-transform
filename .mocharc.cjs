8c242b4fa4e593b2e20e5dffdeb2c0c34c301c7f
Marcelo Shima <marceloshima@gmail.com>
2022-11-28 19:29:19 -0300

    convert to typescript

---
+++ .mocharc.cjs
@@ -0,0 +1,5 @@
+module.exports = {
+  timeout: 3000,
+  extension: ['spec.ts'],
+  'node-option': ['loader=@esbuild-kit/esm-loader'],
+};
--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,46 @@
 import debug from 'debug';
-import PQueue from 'p-queue';
+import PQueue, { type Options as PQueueOptions} from 'p-queue';
 
 import { promisify } from 'util';
-import { pipeline as _pipeline, Transform } from 'stream';
+import { pipeline as _pipeline, Transform, type TransformCallback } from 'stream';
 
 /**
  * Promisified pipeline
  */
 export const pipeline = promisify(_pipeline);
 
+export type TransformMethod = (this: Transform, chunk: any, encoding?: BufferEncoding) => unknown | Promise<unknown>;
+
+export type PTransformOptions = {
+  logPrefix?: string;
+  logName?: string;
+  queueOptions?: PQueueOptions<any, any>;
+  transform: TransformMethod;
+};
+
 export default class PTransform extends Transform {
+  protected logName: string;
+  protected queue: PQueue<any, any>;
+  private _debug: any;
+  private promisedTransform: TransformMethod;
+
   /**
    * PTransform
    *
-   * @param {Object} [options] - Options object forwarded to Transform.
-   * @param {String} [options.logName] - Custom name for logger.
-   * @param {Function} [options.transform] - Transform function.
-   * @param {Object} [options.queueOptions] - Options forwarded to PQueue instance.
+   * @param options - Options object forwarded to Transform.
    */
-  constructor(options = {}) {
+  constructor(options: PTransformOptions) {
     // transform is used locally, forward undefined to prevent conflicts.
-    super({ objectMode: true, ...options });
+    super({ objectMode: true, ...options, transform: undefined });
 
     const {
       logPrefix,
       logName = `${logPrefix ? logPrefix + '-' : ''}${Math.random().toString(36).slice(7)}`,
       queueOptions,
+      transform: promisedTransform,
     } = options;
 
+    this.promisedTransform = promisedTransform;
     this.logName = logName;
     this.queue = new PQueue(queueOptions);
   }
@@ -44,7 +57,7 @@ export default class PTransform extends Transform {
     return this;
   }
 
-  get debug() {
+  private get debug() {
     if (this._debug) return this._debug;
     this._debug = debug(`p-transform:${this.logName}`);
 
@@ -72,7 +85,7 @@ export default class PTransform extends Transform {
    *
    * @return Promise<void>
    */
-  flushQueue() {
+  private flushQueue() {
     return this.queue.onIdle();
   }
 
@@ -83,48 +96,41 @@ export default class PTransform extends Transform {
    * @param {String} encoding
    * @return Promise
    */
-  async queuedTransform(chunk, encoding) {
+  private async queuedTransform(chunk, encoding) {
     try {
-      const maybeChunk = await this._transform(chunk, encoding);
+      const maybeChunk = await this.promisedTransform(chunk, encoding);
       if (maybeChunk) {
         this.push(maybeChunk);
       }
     } catch (error) {
       this.debug('destroying %s', error);
-      this.destroy(error);
+      this.destroy(error as Error);
     }
   }
 
-  _write(chunk, enc, callback) {
-    this.debug('_transform %s', chunk.path);
-    this.queue.add(() => this.queuedTransform(chunk, enc));
+  _write(chunk: any, encoding: BufferEncoding, callback: (error?: Error | null) => void): void {    this.debug('_transform %s', chunk.path);
+    this.queue.add(() => this.queuedTransform(chunk, encoding));
     setTimeout(() => callback());
   }
 
-  _flush(callback) {
+  _flush(callback: TransformCallback): void {
     this.debug('_flush');
-    this.flushQueue().then(() => callback());
+    this.flushQueue().then(() => callback(), callback);
   }
 }
 
 /**
  * Shortcut to create a PTransform with transform and logName.
- *
- * @param {Function} transform
- * @param {String} logName
  */
-export const transform = (transform, logName) => new PTransform({ transform, logName, logPrefix: 'transform' });
+export const transform = (transform: TransformMethod, logName?: string) => new PTransform({ transform, logName, logPrefix: 'transform' });
 
 /**
  * Shortcut to create a passthrough PTransform with spy and logName.
- *
- * @param {Function} spy
- * @param {String} logName
  */
-export const passthrough = (spy = () => {}, logName) =>
+export const passthrough = (spy?: (this: Transform, chunk: any, encoding?: BufferEncoding) => void | Promise<void>, logName?: string) =>
   new PTransform({
     transform: async function (chunk, encoding) {
-      await spy.call(this, chunk, encoding);
+      await spy?.call(this, chunk, encoding);
       return chunk;
     },
     logName,
@@ -133,11 +139,8 @@ export const passthrough = (spy = () => {}, logName) =>
 
 /**
  * Shortcut to create a filter PTransform with filter and logName.
- *
- * @param {Function} filter
- * @param {String} logName
  */
-export const filter = (filter = () => {}, logName) =>
+export const filter = (filter: (this: Transform, chunk: any, encoding?: BufferEncoding) => boolean | Promise<boolean>, logName?: string) =>
   new PTransform({
     transform: async function (chunk, encoding) {
       return (await filter.call(this, chunk, encoding)) ? chunk : undefined;
